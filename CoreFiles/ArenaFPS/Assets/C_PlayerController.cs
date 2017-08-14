52892a6b771132c92d442499afed35939b7f8b02
Chris Christensen <unclesam098@gmail.com>
2017-08-13 20:14:00 -0700

    Player Input
    - Basic Tweaks

--- CoreFiles/ArenaFPS/Assets/C_PlayerController.cs
+++ CoreFiles/ArenaFPS/Assets/C_PlayerController.cs
@@ -9,6 +9,7 @@ public class C_PlayerController : C_INPUT_MANAGER
     Rigidbody this_RigidBody;
     GameObject this_CameraObject;
     Camera this_Camera;
+    GameObject this_RaycastPoint;
 
     // Character Movement
     [SerializeField] float f_MaxSpeed = 5.0f;
@@ -23,12 +24,17 @@ public class C_PlayerController : C_INPUT_MANAGER
         this_RigidBody = this_GameObject.GetComponent<Rigidbody>();
         this_CameraObject = this_GameObject.transform.Find("Camera").gameObject;
         this_Camera = this_CameraObject.GetComponent<Camera>();
+        this_RaycastPoint = this_GameObject.transform.Find("RaycastPoint").gameObject;
 
         base.Start();
 	}
+
     float f_VertAngle;
     void PlayerInput()
     {
+        // Capture player gravity velocity
+        float f_yVel_ = this_RigidBody.velocity.y;
+
         // Rotate player based on Right Stick
         Vector3 v3_PlayerRot = this_GameObject.transform.eulerAngles;
         Vector3 v3_NewRot = v3_PlayerRot;
@@ -45,12 +51,34 @@ public class C_PlayerController : C_INPUT_MANAGER
         this_CameraObject.transform.eulerAngles = v3_CamEuler;
 
         // Convert left Stick into velocity
-        this_RigidBody.velocity = this_GameObject.transform.rotation * new Vector3(playerInput.xDir, 0, playerInput.zDir) * f_MaxSpeed;
+        Vector3 v3_NewVel_ = this_GameObject.transform.rotation * new Vector3(playerInput.xDir, 0, playerInput.zDir) * f_MaxSpeed;
+        v3_NewVel_.y = f_yVel_;
+        this_RigidBody.velocity = v3_NewVel_;
+        
+    }
+
+    bool b_IsTouchingGround;
+    void RaycastToGround()
+    {
+        RaycastHit hit_;
+        int i_LayerMask_ = LayerMask.GetMask("Ground");
+        float f_DistToGround_ = 0.05f;
+        b_IsTouchingGround = false;
+
+        if(Physics.Raycast(this_RaycastPoint.transform.position, Vector3.down, out hit_, f_DistToGround_, i_LayerMask_))
+        {
+            b_IsTouchingGround = true;
+        }
+
+        print("Touching Ground: " + b_IsTouchingGround);
+
+        this_RigidBody.useGravity = !b_IsTouchingGround;
     }
 
     private void FixedUpdate()
     {
         PlayerInput();
+        RaycastToGround();
     }
 
     // Update is called once per frame
